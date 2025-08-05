60eb487fa76e8bed2fc9886e3e115061189e2eec
Perfectmodels <203891273+Perfectmodels@users.noreply.github.com>
2025-08-05 19:46:57 +0100

    Add next.config.ts 1754419615861

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,21 @@ const nextConfig: NextConfig = {
         hostname: 'images.pexels.com',
         pathname: '/photos/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cf.bstatic.com',
+        pathname: '/xdata/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.banturestaurantgabon.com',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/workspace-*/**',
+      },
     ],
   },
 }
